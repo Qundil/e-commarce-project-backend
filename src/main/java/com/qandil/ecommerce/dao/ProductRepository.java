120cb58614ec7d59bdf55c9c4c330357b552dc47
Qundil <70232474+Qundil@users.noreply.github.com>
2021-11-13 04:11:28 +0200

    Add files via upload

---
+++ src/main/java/com/qandil/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.qandil.ecommerce.dao;
+
+
+import com.qandil.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository <Product, Long> {
+
+        //SELECT * FROM Product WHERE category_id=?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+       //SELECT * FROM Product p WHERE p.name LIKE CONCAT ('%', :name ,'%')
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
+}
